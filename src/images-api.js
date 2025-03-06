95bda2846f373a5019003db4d3a5d4dd1274a254
Sfilosha <sfilosha@gmail.com>
2025-03-06 20:36:30 +0200

    clear api pixabay

--- src/images-api.js
+++ src/images-api.js
@@ -1,27 +1,9 @@
 import axios from "axios";
 
-// PIXABAY BELOW
-
-// axios.defaults.baseURL = "https://pixabay.com/api/";
-const key = "10224742-53a1a7880f946c3462445b43a";
-
-export const fetchImagesPixabay = async (query, page, perPage) => {
-  const response = await axios.get("/?", {
-    params: { key, q: query, page, per_page: perPage },
-  });
-  return {
-    hits: response.data.hits,
-    totalPages: response.data.total,
-  };
-};
-
-// UNSPLASH BELOW
-
 axios.defaults.baseURL = "https://api.unsplash.com/search/photos";
 const clientId = "93OoqF-JBpYinb9MpOE6KneWhhdIrNwEijJ9VMnNhKk";
 
 //api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
-
 export const fetchImagesUnsplash = async (query, page, perPage) => {
   const { data } = await axios.get("/?", {
     params: { client_id: clientId, query: query, page, per_page: perPage },
@@ -29,4 +11,4 @@ export const fetchImagesUnsplash = async (query, page, perPage) => {
   return data;
 };
 
-export default { fetchImagesUnsplash, fetchImagesPixabay };
+export default { fetchImagesUnsplash };
